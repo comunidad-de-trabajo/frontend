5ac1ecf29918d39afb9ae41cb7bc955919dcbc62
kevinmatias120 <acastakev888@gmail.com>
2021-04-18 11:57:50 -0300

    display tipo empresa ok

--- src/components/TipoEmpresa.js
+++ src/components/TipoEmpresa.js
@@ -3,57 +3,84 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import { MenuItem } from '@material-ui/core';
+import CameraAltOutlinedIcon from '@material-ui/icons/CameraAltOutlined';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 export default function TipoEmpresa() {
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Payment method
+      <Typography align="center" variant="h6" gutterBottom>
+        Tipo de Empresa
       </Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12}>
           <TextField
             required
-            id="cardName"
-            label="Name on card"
+            id="Areas de interes"
+            label="Areas de interes"
             fullWidth
-            autoComplete="cc-name"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-          />
+            select
+          >
+            <MenuItem value="Administración">Administración</MenuItem>
+            <MenuItem value="Producción">Producción</MenuItem>
+            <MenuItem value="Finanzas y Contabilidad">
+              Finanzas y Contabilidad
+            </MenuItem>
+            <MenuItem value="Marketing">Marketing</MenuItem>
+            <MenuItem value="Informática">Informática</MenuItem>
+            <MenuItem value="Servicios">Servicios</MenuItem>
+            <MenuItem value="Turismo">Turismo</MenuItem>
+            <MenuItem value="Salud">Salud</MenuItem>
+            <MenuItem value="Educación">Educación</MenuItem>
+            <MenuItem value="Rehabilitación">Rehabilitación</MenuItem>
+            <MenuItem value="Ambiente">Ambiente</MenuItem>
+            <MenuItem value="Sustentabilidad">Sustentabilidad</MenuItem>
+            <MenuItem value="Inclusión social ">Inclusión social </MenuItem>
+            <MenuItem value="Diseño">Diseño</MenuItem>
+            <MenuItem value="Arte">Arte</MenuItem>
+            <MenuItem value="Cultura">Cultura</MenuItem>
+            <MenuItem value="Actividad Fisica">Actividad Fisica</MenuItem>
+            <MenuItem value="Deporte">Deporte</MenuItem>
+          </TextField>
         </Grid>
         <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
+          <TextField required id="Sitio web" label="Sitio web" fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField required id="Email" label="Email" fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField required id="Tipo" label="Tipo" fullWidth select>
+            <MenuItem value="Emprendimiento">Emprendimiento</MenuItem>
+            <MenuItem value="PYME">PYME</MenuItem>
+            <MenuItem value="Empresa">Empresa</MenuItem>
+            <MenuItem value="Industria">Industria</MenuItem>
+            <MenuItem value="Agencia">Agencia</MenuItem>
+          </TextField>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="Descripcion"
+            label="Descripcion"
+            multiline
+            fullWidth
+            helperText="Descripción general de la empresa. Puedo incluir datos sobre su história, 
+            Equipo directivo y / o de trabajo Misión, Visión y Valores.Productos o servicios 
+            que brinda."
           />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <button>
+            <Typography align="center">Logo de Empresa</Typography>
+            <CameraAltOutlinedIcon fontSize="large" />
+          </button>
+          <Typography variant="inherit" display="block" color="textSecondary">
+            El logo debe ser formato JPG o PNG, con peso maximo de 500kb y un
+            tamaño minimo de 200px por 200px y maximo de 500px por 500px.
+          </Typography>
         </Grid>
       </Grid>
     </React.Fragment>
