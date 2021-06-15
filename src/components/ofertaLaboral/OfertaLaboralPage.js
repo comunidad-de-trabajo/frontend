735e3a07ff89902b8d794c7d6b7610bacf8b07d1
Gaianimagali <magaliantonella.gaiani@gmail.com>
2021-06-15 16:41:31 -0300

    Estructura general

--- src/components/ofertaLaboral/OfertaLaboralPage.js
+++ src/components/ofertaLaboral/OfertaLaboralPage.js
@@ -5,10 +5,23 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
-import { Grid, FormControl, InputLabel, Select } from '@material-ui/core';
+import {
+  Grid,
+  FormControl,
+  InputLabel,
+  Select,
+  createMuiTheme,
+  withStyles,
+  Box,
+  Button,
+} from '@material-ui/core';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { DatosOferta } from './DatosOferta';
+import { RequisitosOferta } from './RequisitosOferta';
+import { CondicionesOferta } from './CondicionesOferta';
+import { ResponsableBusquedaOferta } from './ResponsableBusquedaOferta';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -19,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-      width: 600,
+      width: 800,
       marginLeft: 'auto',
       marginRight: 'auto',
     },
@@ -45,346 +58,61 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(3),
     marginLeft: theme.spacing(1),
   },
-  textArea: {
-    borderBottom: 'none !important',
-  },
 }));
+
+const GlobalCss = withStyles({
+  '@global': {
+    '.MuiFilledInput-underline:before': {
+      borderBottom: 'none',
+      content: 'none',
+    },
+    '.MuiInputBase-root': {
+      fontSize: '0.8rem',
+    },
+  },
+})(() => null);
 
 export function OfertaLaboralPage() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
+    <>
       <CssBaseline />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography component="h1" variant="h4" align="center">
+          <Typography component="h1" variant="h5" align="center">
             Publicar oferta laboral
           </Typography>
-          <>
-            <Grid container spacing={3}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  id="fechaVigencia"
-                  name="fechaVigencia"
-                  label="Fecha de vigencia"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  id="tituloBusqueda"
-                  name="tituloBusqueda"
-                  label="Titulo de la busqueda"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  id="descripcionEmpresa"
-                  name="descripcionEmpresa"
-                  label="Descripción de la empresa"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField id="mision" name="mision" label="Mision" fullWidth />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  id="respoPrincipales"
-                  name="respoPrincipales"
-                  label="Responsabilidades principales"
-                  fullWidth
-                />
-              </Grid>
+          <Grid
+            container
+            spacing={3}
+            justify="center"
+            alignItems="center"
+            style={{ marginTop: '15px' }}
+          >
+            <DatosOferta />
 
-              <Grid item xs={12}>
-                <Typography variant="subtitle2" gutterBottom>
-                  Requisitos:
-                </Typography>
-                <Typography variant="caption" gutterBottom>
-                  Marque si/no segun corresponda.
-                </Typography>
-              </Grid>
-              <Grid item xs={24} sm={12}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Grid item xs={12} sm={6}>
-                    <Typography variant="caption">
-                      Estudios secundarios completos:
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <FormControl component="fieldset">
-                      <RadioGroup
-                        row
-                        aria-label="position"
-                        name="position"
-                        defaultValue="top"
-                        style={{ marginLeft: '10px' }}
-                      >
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          label={<Typography variant="caption">Si</Typography>}
-                          labelPlacement="top"
-                        />
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          label={<Typography variant="caption">No</Typography>}
-                          labelPlacement="top"
-                        />
-                      </RadioGroup>
-                    </FormControl>
-                  </Grid>
-                </div>
-              </Grid>
-              <Grid item xs={24} sm={12}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Grid item xs={12} sm={6}>
-                    <Typography variant="caption">
-                      Conociemientos de paquete office:
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <FormControl component="fieldset">
-                      <RadioGroup
-                        row
-                        aria-label="position"
-                        name="position"
-                        defaultValue="top"
-                        style={{ marginLeft: '10px' }}
-                      >
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                      </RadioGroup>
-                    </FormControl>
-                  </Grid>
-                </div>
-              </Grid>
-              <Grid item xs={24} sm={12}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Grid item xs={12} sm={6}>
-                    <Typography variant="caption">
-                      Licencia de conducir:
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <FormControl component="fieldset">
-                      <RadioGroup
-                        row
-                        aria-label="position"
-                        name="position"
-                        defaultValue="top"
-                        style={{ marginLeft: '10px' }}
-                      >
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                      </RadioGroup>
-                    </FormControl>
-                  </Grid>
-                </div>
-              </Grid>
-              <Grid item xs={24} sm={12}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Grid item xs={12} sm={6}>
-                    <Typography variant="caption">Edad:</Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <FormControl component="fieldset">
-                      <RadioGroup
-                        row
-                        aria-label="position"
-                        name="position"
-                        defaultValue="top"
-                        style={{ marginLeft: '10px' }}
-                      >
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                      </RadioGroup>
-                    </FormControl>
-                  </Grid>
-                </div>
-              </Grid>
+            <RequisitosOferta />
 
-              <Grid item xs={3} sm={3}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography variant="caption">Desde:</Typography>
-                  <TextField
-                    id="filled-basic"
-                    variant="filled"
-                    size="small"
-                    style={{
-                      marginLeft: '10px',
-                      paddingTop: 'none !important',
-                      paddingBottom: 'none !important',
-                      padding: 'none !important',
-                    }}
-                  />
-                </div>
-              </Grid>
-              <Grid item xs={3} sm={3}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography variant="caption">Hasta:</Typography>
-                  <TextField
-                    id="filled-basic"
-                    variant="filled"
-                    size="small"
-                    style={{ marginLeft: '10px' }}
-                  />
-                </div>
-              </Grid>
-              <Grid item xs={24} sm={12}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Grid item xs={12} sm={6}>
-                    <Typography variant="caption">
-                      Experiencia previa:
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <FormControl
-                      component="fieldset"
-                      className={classes.textArea}
-                    >
-                      <RadioGroup
-                        row
-                        aria-label="position"
-                        name="position"
-                        defaultValue="top"
-                        style={{ marginLeft: '10px' }}
-                      >
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                        <FormControlLabel
-                          value="top"
-                          control={<Radio color="primary" />}
-                          labelPlacement="top"
-                        />
-                      </RadioGroup>
-                    </FormControl>
-                  </Grid>
-                </div>
-              </Grid>
+            <CondicionesOferta />
 
-              <Grid item xs={12}>
-                <TextField
-                  id="filled-multiline-static"
-                  placeholder="Espacio para completar más detalles"
-                  multiline
-                  fullWidth
-                  rows={2}
-                  variant="filled"
-                />
-              </Grid>
+            <ResponsableBusquedaOferta />
+          </Grid>
 
-              <Grid item xs={12}>
-                <Typography variant="caption" gutterBottom>
-                  Complete los campos requeridos.
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12}>
-                <Typography variant="caption" gutterBottom>
-                  Residencia:
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  id="filled-multiline-static"
-                  placeholder="Indicar zona"
-                  multiline
-                  fullWidth
-                  rows={1}
-                  variant="filled"
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Typography variant="caption" gutterBottom>
-                  Capacitación acreditada en áreas de estudio como:
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  id="filled-multiline-static"
-                  placeholder="Complete"
-                  multiline
-                  fullWidth
-                  rows={1}
-                  variant="filled"
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Typography variant="caption" gutterBottom>
-                  Competencias:
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  id="filled-multiline-static"
-                  placeholder="Complete"
-                  multiline
-                  fullWidth
-                  rows={1}
-                  variant="filled"
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Typography variant="caption" gutterBottom>
-                  Otros detalles:
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  id="filled-multiline-static"
-                  placeholder="Detalles"
-                  multiline
-                  fullWidth
-                  rows={2}
-                  variant="filled"
-                />
-              </Grid>
-            </Grid>
-          </>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{
+              marginTop: '15px',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            Publicar
+          </Button>
         </Paper>
       </main>
-    </React.Fragment>
+      <GlobalCss />
+    </>
   );
 }
