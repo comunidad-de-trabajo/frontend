d6e476dd4d009634224a7a932a8e77b6eb613f32
leonelmperalta <leonelm.peralta@outlook-es>
2021-12-08 13:10:38 -0300

    arreglado get de ofertas por empresa

--- src/services/listadoDeOfertas/endpointListadoOfertaXEmpresa.js
+++ src/services/listadoDeOfertas/endpointListadoOfertaXEmpresa.js
@@ -2,9 +2,9 @@ import api from '../envio-mails-mailjet/api';
 
 export async function fetchListadoOfertas() {
   /*en realidad no se filtra nada es solo para que el if del backend me lo tome*/
-  const response = await api.get('api/oferta/listar?usuario=hola', {
-    token: localStorage.getItem('token'),
-  });
+  const response = await api.get(
+    'api/oferta/listar?usuario=' + localStorage.getItem('token')
+  );
   return response.data;
 }
 
